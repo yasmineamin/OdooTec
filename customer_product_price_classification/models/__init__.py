371c59e4341e503dc381db9c204b5bab99110d18
Yasmin <yasmin2012653@miuegypt.edu.eg>
2025-01-29 09:29:52 +0200

    removing the folders, to create a git ignore file

--- customer_product_price_classification/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import res_partner
-from . import product_template
-from . import sale_order_line
--- customer_product_price_classification/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    customer_class = fields.Selection(
-        [('A', 'Class A'), ('B', 'Class B'), ('C', 'Class C')],
-        string="Customer Class"
-    )
